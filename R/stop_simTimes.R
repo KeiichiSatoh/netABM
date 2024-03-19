2ac84f72ee3b79d3b973b2209e17d10f84c0d383
huhn1234 <k.s.vogel1234@gmail.com>
2024-03-19 12:18:35 +0900

    Amended the description of stop_simTimes

--- R/stop_simTimes.R
+++ R/stop_simTimes.R
@@ -1,9 +1,9 @@
-#' @title Stop the ABM Simulation by Runtime
+#' @title Stop ABM Simulation by Runtime
 #' @description
-#' \code{stopABM_times} returns TRUE when the ABM simulation runs after the defined time.
+#' \code{stopABM_times} returns TRUE when the ABM simulation continues after the defined time.
 #' @param D The D object. Note that this will be supplied internally within \code{runABM}. Therefore, user needs to leave this argument as it is.
 #' @param simTimes Number of run (Default: 1)
-#' @returns TRUE (when run-time reaches the specified time)
+#' @returns  TRUE (if the simulation has run for the defined number of times specified by simTimes)
 #' @family stopABM
 #' @author Keiichi Satoh
 #' @export
